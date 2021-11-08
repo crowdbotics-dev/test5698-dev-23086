44f60042c2a655a409a7627d6bdd9875c77bf3c7
Crowdbotics <engineering@crowdbotics.com>
2021-11-08 17:24:39 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native"
 const styles = StyleSheet.create({
   hr: {
-    marginTop: 20,
+    marginTop: 10,
     marginBottom: 20,
     borderBottomColor: "black",
     borderBottomWidth: 1
