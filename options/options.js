f679d560633f573ed31040c13d14fcf4f40b24c3
Crowdbotics <engineering@crowdbotics.com>
2021-11-08 16:18:15 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -1,12 +1,29 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
+const styles = StyleSheet.create({
+  hr: {
+    marginTop: 40,
+    marginBottom: 20,
+    borderBottomColor: "black",
+    borderBottomWidth: 1
+  },
+  container: { flex: 1, height: 100, padding: 13 },
+  text: { color: "black", fontSize: 20 },
+  buttonPressed: { backgroundColor: "aquamarine" },
+  buttonNotPressed: { backgroundColor: "blue" },
+  button: { borderRadius: 4, padding: 15, marginTop: 10 },
+  buttonText: { color: "white", textAlign: "center", fontSize: 16 }
+})
 
 export const globalOptions = {
   name: "test5698_dev_23086",
   url: "https://test5698_dev_23086.botics.co",
   api: "https://test5698_dev_23086.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    title: "App Menu",
+    copy: "Routes available!",
+    styles: styles
   }
 }
